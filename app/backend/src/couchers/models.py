2db9af15dfbf383b91922a3345464058bee7609c
Krishnag09 <krishnag0902@gmail.com>
2025-07-14 23:18:16 -0400

    ruff fix

--- app/backend/src/couchers/models.py
+++ app/backend/src/couchers/models.py
@@ -180,15 +180,11 @@ class User(Base):
     last_active = Column(DateTime(timezone=True), nullable=False, server_default=func.now())
 
     heard_about_us_source = Column(
-        Enum(HeardAboutUs), 
+        Enum(HeardAboutUs),
         server_default=text("'source_friend'"),  # Use the string representation of the Enum value
-        nullable=False
+        nullable=False,
     )
-    heard_about_us_source_other = Column(
-        String, 
-        nullable=True, 
-        server_default=text("''")
-    )
+    heard_about_us_source_other = Column(String, nullable=True, server_default=text("''"))
 
     public_visibility = Column(Enum(ProfilePublicVisibility), nullable=False, server_default="map_only")
     has_modified_public_visibility = Column(Boolean, nullable=False, server_default=expression.false())
